037f4838de8a9e4e8f4b17c45f00812474097b6d
Tony Trinh <tony19@gmail.com>
2021-05-04 17:20:23 -0500

    Remove runtime config for ignoring custom elements
    That config is unnecessary since vue-loader is configured at compile time to ignore `bdi-*` elements.

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import router from './router'
 //createApp(App).use(router).mount('#app')
 
 const app = createApp(App)
-//runtime config to ignore bdl elements, non-runtime config somewhere to ...
-app.config.isCustomElement = (tag) => tag.startsWith('bdl-');
-app.config.ignoredElements = ['bdl-fmi','bdl-chartjs-time','bdl-range']
 app.use(router).mount('#app')
 
 /*
